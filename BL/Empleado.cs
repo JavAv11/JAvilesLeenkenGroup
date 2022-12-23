8064f35c5588cec316d82e35210dd538e3699799
JavAv11 <Javcoria01@gmail.com>
2022-12-22 18:10:54 -0600

    Add project files.

---
+++ BL/Empleado.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Empleado
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.JavilesLeenkenGroupContext context = new DL.JavilesLeenkenGroupContext())
+                {
+                    var query = context.Empleados.FromSqlRaw($"GetAllEmpleado").ToList();
+
+                    result.Objects = new List<object>();
+
+                    if (query != null)
+                    {
+                        foreach (var objEmpleado in query)
+                        {
+                            ML.Empleado empleado = new ML.Empleado();
+
+                            empleado.IdEmpleado = objEmpleado.IdEmpleado;
+                            empleado.Nombre = objEmpleado.Nombre;
+                            empleado.ApellidoPaterno = objEmpleado.ApellidoPaterno;
+                            empleado.ApellidoMaterno = objEmpleado.ApellidoMaterno;
+                            empleado.NumeroNomina = objEmpleado.NumeroNomina;
+
+                            empleado.Estado = new ML.Estado();
+                            empleado.Estado.IdEstado = objEmpleado.IdEstado.Value;
+
+                            result.Objects.Add(empleado);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+
+        public static ML.Result GetById(int IdEmpleado)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.JavilesLeenkenGroupContext context = new DL.JavilesLeenkenGroupContext())
+                {
+                    var query = context.Empleados.FromSqlRaw($"GetByIdEmpleado {IdEmpleado}").AsEnumerable().FirstOrDefault();
+                    if(query != null)
+                    {
+                        ML.Empleado objEmpleado = new ML.Empleado();
+                        objEmpleado.IdEmpleado = query.IdEmpleado;
+                        objEmpleado.Nombre = query.Nombre;
+                        objEmpleado.ApellidoPaterno =   query.ApellidoPaterno;
+                        objEmpleado.ApellidoMaterno = query.ApellidoMaterno;
+                        objEmpleado.NumeroNomina = query.NumeroNomina;
+
+                        objEmpleado.Estado = new ML.Estado();
+                        objEmpleado.Estado.IdEstado = query.IdEstado.Value;
+
+                        result.Object = objEmpleado;
+
+                        result.Correct=true;
+                    }
+                    else
+                    {
+                        result.Correct=false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+
+        public static ML.Result Add(ML.Empleado empleado)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using(DL.JavilesLeenkenGroupContext context = new DL.JavilesLeenkenGroupContext())
+                {
+                    var query = context.Database.ExecuteSqlRaw($"AddEmpleado '{empleado.Nombre}','{empleado.ApellidoPaterno}','{empleado.ApellidoMaterno}',{empleado.Estado.IdEstado}");
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct  =   false;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        public static ML.Result Update(ML.Empleado empleado)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.JavilesLeenkenGroupContext context = new DL.JavilesLeenkenGroupContext())
+                {
+                    var query = context.Database.ExecuteSqlRaw($"UpdateEmpleado {empleado.IdEmpleado},'{empleado.Nombre}','{empleado.ApellidoPaterno}','{empleado.ApellidoMaterno}',{empleado.Estado.IdEstado}");
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+
+        public static ML.Result Delete(int IdEmpleado)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.JavilesLeenkenGroupContext context = new DL.JavilesLeenkenGroupContext())
+                {
+                    var query = context.Empleados.FromSql($"DeleteEmpleado {IdEmpleado}").AsEnumerable().FirstOrDefault();
+                    if (query != null)
+                    {
+                        result.Correct = false;
+                    }
+                    else
+                    {
+
+                        result.Correct = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ SL/Controllers/EmpleadoController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            ML.Empleado empleado = new ML.Empleado();
+
+            empleado.Estado = new ML.Estado();
+            ML.Result result = BL.Empleado.GetAll();
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet("GetById/{IdEmpleado}")]
+        public IActionResult Get(int IdEmpleado)
+        {
+            ML.Empleado empleado = new ML.Empleado();
+
+            empleado.Estado = new ML.Estado();
+            ML.Result result = BL.Empleado.GetById(IdEmpleado);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete ("Delete/{idUsuario}")]
+        public IActionResult Delete(int IdEmpleado)
+        {
+            ML.Result result = new ML.Result();
+
+            result = BL.Empleado.Delete(IdEmpleado);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("Add")]
+        public ActionResult Add(ML.Empleado empleado)
+        {
+            ML.Result result = new ML.Result();
+
+
+            result = BL.Empleado.Add(empleado);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("Update")]
+        public ActionResult Update(ML.Empleado empleado)
+        {
+            ML.Result result = new ML.Result();
+
+            result = BL.Empleado.Update(empleado);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
